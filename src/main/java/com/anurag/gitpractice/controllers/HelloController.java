dbb2d74c6d91183217f1dae7e41425500f67a1a4
anuragkumar2437 <anuragkumar2437@gmail.com>
2026-02-13 19:43:52 +0530

    Added HelloController

---
+++ src/main/java/com/anurag/gitpractice/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.anurag.gitpractice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/v1/api/")
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String getMethodName() {
+		return "Welcome to Hello COtroller";
+	}
+	
+}
